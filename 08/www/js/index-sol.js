ed7bf69b2cdc12295b4d4e44124123cb51954378
Christina <sweetflute@gmail.com>
2019-09-23 16:33:51 -0400

    update exercise solution

---
+++ 08/www/js/index-sol.js
@@ -0,0 +1,94 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+/*** Geolocation ***/
+function onGeolocationError(error){
+    // alert("onGeolocationError");
+    // alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
+
+    $("#error-info").append("<div class='row'><div class='col-sm'><strong>Geolocation Error</strong></div></div>");
+    $("#error-info").append("<div class='row'><div class='col-sm'>Error code = " + error.code + "</div></div>");
+    $("#error-info").append("<div class='row'><div class='col-sm'>Error message = " + error.message + "</div></div>");
+
+}
+
+function onGeolocationSuccess(position){
+    // alert("onGeolocationSuccess");
+    // alert('Latitude: '          + position.coords.latitude          + '\n' +
+    //       'Longitude: '         + position.coords.longitude         + '\n' +
+    //       'Altitude: '          + position.coords.altitude          + '\n' +
+    //       'Accuracy: '          + position.coords.accuracy          + '\n' +
+    //       'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+    //       'Heading: '           + position.coords.heading           + '\n' +
+    //       'Speed: '             + position.coords.speed             + '\n' +
+    //       'Timestamp: '         + position.timestamp                + '\n'
+    // );
+
+    
+    $("#latitude").text(position.coords.latitude);
+    $("#longitude").text(position.coords.longitude);
+    $("#altitute").text(position.coords.altitude);
+    $("#accuracy").text(position.coords.accuracy);
+    $("#altitudeAccuracy").text(position.coords.altitudeAccuracy);
+    $("#heading").text(position.coords.heading);
+    $("#speed").text(position.coords.speed);
+    $("#timestamp").text(position.timestamp);
+
+    $("#success-info").css("display","block");
+
+}
+
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+
+        // === goelocation ===
+        navigator.geolocation.getCurrentPosition(onGeolocationSuccess,
+                                         onGeolocationError,
+                                         { maximumAge: 3000, timeout: 5000, enableHighAccuracy: true });
+
+
+        
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        // var parentElement = document.getElementById(id);
+        // var listeningElement = parentElement.querySelector('.listening');
+        // var receivedElement = parentElement.querySelector('.received');
+
+        // listeningElement.setAttribute('style', 'display:none;');
+        // receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
